d7b5ff2eef83060115fe96a9b5ff75ed4e1ef05b
Ryan Jacobson <ryan@Ryans-MBP.home>
2020-08-11 01:37:17 -0400

    Updates

---
+++ precache-manifest.4e10fb0bc8b25931ec8d52d666be4b53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b49e7540590e188795b8dde3da1c6c",
+    "url": "/sorting-visualizer/index.html"
+  },
+  {
+    "revision": "13e820733af863602120",
+    "url": "/sorting-visualizer/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "d4b61d4d2d4ee9a8f8b2",
+    "url": "/sorting-visualizer/static/css/main.417c687c.chunk.css"
+  },
+  {
+    "revision": "13e820733af863602120",
+    "url": "/sorting-visualizer/static/js/2.22e462df.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/sorting-visualizer/static/js/2.22e462df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4b61d4d2d4ee9a8f8b2",
+    "url": "/sorting-visualizer/static/js/main.b75fd28a.chunk.js"
+  },
+  {
+    "revision": "123b71b70ac2b92fbce2",
+    "url": "/sorting-visualizer/static/js/runtime-main.4aded140.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sorting-visualizer/precache-manifest.050cfcb70d2943219d506e825c58a09f.js"
+  "/sorting-visualizer/precache-manifest.4e10fb0bc8b25931ec8d52d666be4b53.js"
 );
 
 self.addEventListener('message', (event) => {
